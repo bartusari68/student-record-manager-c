2a831a8f197e414bc89aaee68c8efd9e777e1140
Bartu Sarı <bartusari68@gmail.com>
2026-02-25 03:44:48 +0300

    feat: add update and delete options to main menu
    Extend menu flow to support updating and deleting student records by ID.

--- main.c
+++ main.c
@@ -13,8 +13,10 @@ int main() {
         printf("1. Add Student\n");
         printf("2. List Students\n");
         printf("3. Search Student by ID\n");
-        printf("4. Save Records\n");
-        printf("5. Exit\n");
+        printf("4. Update Student by ID\n");
+        printf("5. Delete Student by ID\n");
+        printf("6. Save Records\n");
+        printf("7. Exit\n");
         printf("Choose an option: ");
         scanf("%d", &choice);
 
@@ -29,16 +31,22 @@ int main() {
                 searchStudentById(students, count);
                 break;
             case 4:
-                saveToFile(students, count);
+                updateStudentById(students, count);
                 break;
             case 5:
+                deleteStudentById(students, &count);
+                break;
+            case 6:
+                saveToFile(students, count);
+                break;
+            case 7:
                 saveToFile(students, count);
                 printf("Exiting program...\n");
                 break;
             default:
                 printf("Invalid choice. Please try again.\n");
         }
-    } while (choice != 5);
+    } while (choice != 7);
 
     return 0;
 }
